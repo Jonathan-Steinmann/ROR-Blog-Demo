d45b91b6379790d874caeabf4b7c742c5cfb43c8
CMI Web Studio <info@cmiwebstudio.com>
2012-11-09 21:58:07 -0500

    ROR Blog Demo

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'fc83d1da97dd8d09250069002287b30fd9ca6cc9719bb8d24e49d498f04c1c83b3c417dea56e1068c8cd870a0ca9fb9468dfe8992fd09b03cf611cd01e23884a'
